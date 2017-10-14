804dcd724f84a2903d843c8ad7cdba7f460a6c46
Saskia Reiss <nyotheneko@gmail.com>
2017-10-14 13:40:12 +0200

    Creating and putting function skeletons in implementations

---
+++ project1/implementations.py
@@ -0,0 +1,55 @@
+# Useful imports
+%matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+  """calculates the least squares solution using gradien descent."""
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # least squares GD: TODO
+    # returns ???
+    # ***************************************************
+  raise NotImplementedError
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+  """calculates the least squares solution using stochastic gradien descent."""
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # least squares SGD: TODO
+    # returns ???
+    # ***************************************************
+  raise NotImplementedError
+  
+def least_squares(y, tx):
+  """calculates the least squares solution."""
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # least squares: TODO
+    # returns mse, and optimal weights
+    # ***************************************************
+  raise NotImplementedError
+
+def ridge_regression(y, tx, lambda_):
+  """implements ridge regression."""
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # ridge regression: TODO
+    # ***************************************************
+  raise NotImplementedError
+  
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+  """implements logistic regression using gradient descent or SGD."""
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # ridge regression: TODO
+    # ***************************************************
+  raise NotImplementedError
+  
+def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
+  """implements regularized logistic regression using gradient descent or SGD."""
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # ridge regression: TODO
+    # ***************************************************
+  raise NotImplementedError
