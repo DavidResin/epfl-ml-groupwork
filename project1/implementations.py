7713d71fee1bf8edede6b479adbd3fa29b1af919
Resin <david.resin@epfl.ch>
2017-10-15 19:42:01 +0200

    small corrections

--- project1/implementations.py
+++ project1/implementations.py
@@ -7,50 +7,54 @@ from utility import *
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
   """calculates the least squares solution using gradien descent."""
     # Define parameters to store w and loss
-    ws = [initial_w]
-    losses = []
     w = initial_w
+    ws = [w]
+    losses = []
+    
     for n_iter in range(max_iters):
         
         gradient = compute_gradient(y, tx, w)
         loss = compute_mse(y, tx, w)
         
-        w = w - gamma*gradient
+        w = w - gamma * gradient
         
         # store w and loss
         ws.append(w)
         losses.append(loss)
 
-    return losses, ws
+    return w, losses
 
 def least_squares_SGD(y, tx, initial_w, batch_size, max_iters, gamma):
   """calculates the least squares solution using stochastic gradien descent."""
     # Define parameters to store w and loss
-    ws = [initial_w]
-    losses = []
     w = initial_w
+    ws = [w]
+    losses = []
 
     for n_iter in range(max_iters):
         for y_batch, tx_batch in batch_iter(y, tx, batch_size=batch_size, num_batches=1):
+            
             # compute a stochastic gradient and loss
             gradient, _ = compute_stoch_gradient(y_batch, tx_batch, w)
+            
             # update w through the stochastic gradient update
-            w = w - gamma*gradient
+            w = w - gamma * gradient
+            
             # calculate loss
             loss = compute_mse(y, tx, w)
+            
             # store w and loss
             ws.append(w)
             losses.append(loss)
             
-    return losses, ws
+    return w, losses
   
 def least_squares(y, tx):
   """calculates the least squares solution."""
-    w = np.linalg.solve(tx.T@tx, tx.T@y)
+    w = np.linalg.solve(tx.T @ tx, tx.T @ y)
     loss = compute_mse(y, tx, w)
     
-    return loss, w
-  #raise NotImplementedError
+    return w, loss
 
 def ridge_regression(y, tx, lambda_):
   """implements ridge regression."""
