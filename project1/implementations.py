b07f050224d0529bb74f9d43ecbf396b44cb0fd9
APinCode <alvaropinedo22@yahoo.es>
2017-10-15 19:16:22 +0200

    mis implementation

--- project1/implementations.py
+++ project1/implementations.py
@@ -2,33 +2,55 @@
 %matplotlib inline
 import numpy as np
 import matplotlib.pyplot as plt
+from utility import *
 
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
   """calculates the least squares solution using gradien descent."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares GD: TODO
-    # returns ???
-    # ***************************************************
-  raise NotImplementedError
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        
+        gradient = compute_gradient(y, tx, w)
+        loss = compute_mse(y, tx, w)
+        
+        w = w - gamma*gradient
+        
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
 
-def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+    return losses, ws
+
+def least_squares_SGD(y, tx, initial_w, batch_size, max_iters, gamma):
   """calculates the least squares solution using stochastic gradien descent."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares SGD: TODO
-    # returns ???
-    # ***************************************************
-  raise NotImplementedError
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+
+    for n_iter in range(max_iters):
+        for y_batch, tx_batch in batch_iter(y, tx, batch_size=batch_size, num_batches=1):
+            # compute a stochastic gradient and loss
+            gradient, _ = compute_stoch_gradient(y_batch, tx_batch, w)
+            # update w through the stochastic gradient update
+            w = w - gamma*gradient
+            # calculate loss
+            loss = compute_mse(y, tx, w)
+            # store w and loss
+            ws.append(w)
+            losses.append(loss)
+            
+    return losses, ws
   
 def least_squares(y, tx):
   """calculates the least squares solution."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares: TODO
-    # returns mse, and optimal weights
-    # ***************************************************
-  raise NotImplementedError
+    w = np.linalg.solve(tx.T@tx, tx.T@y)
+    loss = compute_mse(y, tx, w)
+    
+    return loss, w
+  #raise NotImplementedError
 
 def ridge_regression(y, tx, lambda_):
   """implements ridge regression."""
---
+++ project1/utility.py
@@ -0,0 +1,106 @@
+import numpy as np
+
+def compute_mse(y, tx, w):
+    """compute the loss by mse."""
+    e = y - tx.dot(w)
+    mse = e.dot(e) / (2 * len(e))
+    return mse
+
+
+def compute_mae(y, tx, w):
+    """Compute the loss by mae."""
+    e = y - tx.dot(w)
+    mae = e / len(e)
+    return mae
+
+
+def compute_stoch_gradient(y, tx, w):
+    """Compute a stochastic gradient for batch data."""
+    err = y - tx.dot(w)
+    grad = -tx.T.dot(err) / len(err)
+    return grad, err
+
+
+def compute_gradient(y, tx, w):
+    """Compute the gradient."""
+    gradient = -(1/y.shape[0])*np.dot(tx.T, (y - np.dot(tx, w)))
+    return gradient
+
+
+def load_data():
+    """load data."""
+    data = np.loadtxt("dataEx3.csv", delimiter=",", skiprows=1, unpack=True)
+    x = data[0]
+    y = data[1]
+    return x, y
+
+
+def load_data_from_ex02(sub_sample=True, add_outlier=False):
+    """Load data and convert it to the metric system."""
+    path_dataset = "height_weight_genders.csv"
+    data = np.genfromtxt(
+        path_dataset, delimiter=",", skip_header=1, usecols=[1, 2])
+    height = data[:, 0]
+    weight = data[:, 1]
+    gender = np.genfromtxt(
+        path_dataset, delimiter=",", skip_header=1, usecols=[0],
+        converters={0: lambda x: 0 if b"Male" in x else 1})
+    # Convert to metric system
+    height *= 0.025
+    weight *= 0.454
+
+    # sub-sample
+    if sub_sample:
+        height = height[::50]
+        weight = weight[::50]
+
+    if add_outlier:
+        # outlier experiment
+        height = np.concatenate([height, [1.1, 1.2]])
+        weight = np.concatenate([weight, [51.5/0.454, 55.3/0.454]])
+
+    return height, weight, gender
+
+
+def standardize(x):
+    """Standardize the original data set."""
+    mean_x = np.mean(x)
+    x = x - mean_x
+    std_x = np.std(x)
+    x = x / std_x
+    return x, mean_x, std_x
+
+
+def build_model_data(height, weight):
+    """Form (y,tX) to get regression data in matrix form."""
+    y = weight
+    x = height
+    num_samples = len(y)
+    tx = np.c_[np.ones(num_samples), x]
+    return y, tx
+
+
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
