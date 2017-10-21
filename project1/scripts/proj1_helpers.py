1150a2a41b819a2a02977acab134b379b2848814
Saskia Reiss <nyotheneko@gmail.com>
2017-10-21 12:10:08 +0200

    Took out useless dfunctions

--- project1/scripts/proj1_helpers.py
+++ project1/scripts/proj1_helpers.py
@@ -28,41 +28,6 @@ def compute_gradient(y, tx, w):
     return gradient
 
 
-def load_data():
-    """load data."""
-    data = np.loadtxt("dataEx3.csv", delimiter=",", skiprows=1, unpack=True)
-    x = data[0]
-    y = data[1]
-    return x, y
-
-
-def load_data_from_ex02(sub_sample=True, add_outlier=False):
-    """Load data and convert it to the metric system."""
-    path_dataset = "height_weight_genders.csv"
-    data = np.genfromtxt(
-        path_dataset, delimiter=",", skip_header=1, usecols=[1, 2])
-    height = data[:, 0]
-    weight = data[:, 1]
-    gender = np.genfromtxt(
-        path_dataset, delimiter=",", skip_header=1, usecols=[0],
-        converters={0: lambda x: 0 if b"Male" in x else 1})
-    # Convert to metric system
-    height *= 0.025
-    weight *= 0.454
-
-    # sub-sample
-    if sub_sample:
-        height = height[::50]
-        weight = weight[::50]
-
-    if add_outlier:
-        # outlier experiment
-        height = np.concatenate([height, [1.1, 1.2]])
-        weight = np.concatenate([weight, [51.5/0.454, 55.3/0.454]])
-
-    return height, weight, gender
-
-
 def standardize(x):
     """Standardize the original data set."""
     mean_x = np.mean(x)
